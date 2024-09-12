6037b7fac462775c5f8d5a3d30b90f3e152d6297
naveenshri1997 <naveenshri0546@gmail.com>
2024-09-12 09:20:27 +0530

    About update

--- company_website/src/components/About.jsx
+++ company_website/src/components/About.jsx
@@ -1,6 +1,22 @@
 const About = () => {
     return (
-        <h1>hi naveen</h1>
+        <div className="container-fluid home_page_about">
+            <div className="row justify-content-center">
+                <div className="col-lg-12 ">
+                    <div className="row">
+                       
+                        <div className="col-lg-7 home_about_two">
+                            <span className="londrina heading theme_color_two"
+                                style={{ textAlign: 'left', background: 'black', padding: '0px 10px' }}>About Us</span>
+                            <p className="about_title">Welcome to REIWA Technology Systems Pvt Ltd</p>                         
+                        </div>
+                        <div className="col-lg-5 home_about_one" >
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default About
--- company_website/src/pages/Home.jsx
+++ company_website/src/pages/Home.jsx
@@ -50,8 +50,8 @@ const handleDone = () => {
                     </div>
                 </div>
             </div>            
-            <Services />
             <About />
+            <Services />
         </>
     )
 }
