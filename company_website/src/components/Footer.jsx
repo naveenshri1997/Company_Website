43e9f395014ad1ee04fd640a55fba44c98a1d3dd
naveenshri1997 <naveenshri0546@gmail.com>
2024-09-13 11:47:03 +0530

    new upadate on footer

--- company_website/src/components/Footer.jsx
+++ company_website/src/components/Footer.jsx
@@ -9,14 +9,14 @@ const Footer = () => {
     return (
         <>
             <div className="footer container-fluid" style={styleone}>
-                <div className="row justify-content-center" style={{ position: 'relative', borderBottom: '1px solid #616161a8' } }>
+                <div className="row justify-content-center" style={{ position: 'relative' }}>
                     <div className="col-lg-11 ">
-                        <div className="row" >
-                               <h3>QUICK CONTACTS</h3>
+                        <div className="row" style={{ borderBottom: '1px solid #616161a8' }}>
+                            <h3>QUICK CONTACTS</h3>
                             <div className="col-lg-4">
                                 <div className="row footer_box">
                                     <div className=" col-md-auto mt-2">
-                                        <img className="img-fluid footer_small_img" src={location} width="50px"/>
+                                        <img className="img-fluid footer_small_img" src={location} width="50px" />
                                     </div>
                                     <div className=" col-md-auto footer_text"><p>Bhopal,M.P</p></div>
 
@@ -41,8 +41,44 @@ const Footer = () => {
                                 </div>
                             </div>
                         </div>
+                        <div class="row footer_part_two">
+                            <div class="col-lg-4">
+                                <a class="navbar-brand theme_color_two londrina" style={{ fontSize: '30px' }} href="#">LOGO HERE</a>
+                                <p class="footer_para">We have advanced skills & resources to create large-scale solutions as
+                                    well as guide startups from idea to profit. We offer customized software
+                                    development in Indore. Fovty Solutions, a prominent provides Web Development,
+                                    online marketing, cloud marketing, AI, online streaming, etc. Fovty Solutions in
+                                    Indore has merged to serve the education and healthcare industries.</p>
+                            </div>
+                            <div class="col-lg-4">
+                                
+                                <h3 class="para_heading">Services <span>Technologies</span></h3>
+                                <ul>
+                                    <li>React Native</li>
+                                    <li>Angular</li>
+                                    <li>Vue.JS</li>
+                                    <li>React.JS</li>
+                                    <li>iOS</li>
+                                    <li>Android</li>
+                                    <li>Flutter</li>
+                                    <li>Xamarin</li>
+                                </ul>
+                            </div>
+                            <div class="col-lg-4">
+                                <h3 class="para_heading">Useful Links</h3>
+                                <ul>
+                                    <li>Home</li>
+                                    <li>About Us</li>
+                                    <li>Our Services</li>
+                                    <li>Contact Us</li>
+                                    <li>Carrier</li>                                    
+                                </ul>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
+
             </div>
         </>
     )
