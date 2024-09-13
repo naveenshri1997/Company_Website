900a23030e92b82c5ee5f0b82da22cedb239f59e
naveenshri1997 <naveenshri0546@gmail.com>
2024-09-13 19:09:57 +0530

    ew update

--- company_website/src/pages/Home.jsx
+++ company_website/src/pages/Home.jsx
@@ -59,9 +59,8 @@ const handleDone = () => {
             </div>            
             <About />
             <Services />
-            <Steps />
-
             <Review />
+            <Steps />
             <Footer />
         </>
     )
