fb44e9229e129fb3e73e7348f78ef317fb0f4998
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-09-19 22:38:29 +0530

    Update About.jsx

--- company_website/src/components/About.jsx
+++ company_website/src/components/About.jsx
@@ -15,16 +15,18 @@ const About = () => {
                             <h1 data-aos="fade-right" data-aos-duration="1200" className="mobile_heading londrina heading theme_color_two"
                                 style={{ fontWeight: 'bolder ', textAlign: 'left',color:'black', padding: '0px 10px' }}>About <span
                                     style={{ color: '#f84525' }}>Us</span></h1>
-                            <p data-aos="fade-right" data-aos-duration="1200"  className="about_title">Welcome to REIWA Technology Systems Pvt Ltd</p>                         
+                            <p data-aos="fade-right" data-aos-duration="1200"  className="about_title">Welcome to WhiteWeb Tech</p>                         
 
                             <p data-aos="fade-up" data-aos-duration="1200" className="about_title">
-                                At Qualwebs, we distinguish ourselves through a combination of expertise, innovation, and commitment to quality.
-                                Qualwebs harnesses cutting-edge technologies including blockchain, web3, machine learning, and artificial
-                                intelligence to provide innovative solutions for our clients. Backed by an experienced team of IT Engineers
-                                and a robust framework, Qualwebs assists cutting-edge technologies including blockchain, web3, machine learning, and artificial
-                                intelligence to provide innovative solutions for our clients. Backed by an experienced team of IT Engineers
-                                and a robust framework, Qualwebs assists businesses in developing bespoke software and mobile applications
-                                that drive seamless digital transformation.
+                               We are a dynamic IT company offering specialized web and app development 
+                                services designed to bring your business vision to life. With expertise in modern
+                                technologies and a passion for innovation, we create custom websites and mobile
+                                applications that are not only visually appealing but also optimized for
+                                performance and user experience. Our team works closely with clients to 
+                                understand their unique needs, ensuring every project aligns with their business 
+                                objectives. Whether you're looking to enhance your online presence, streamline 
+                                operations, or engage customers, we deliver solutions that drive growth
+                                and long-term success in the digital landscape.
                             </p>
 
                             <Link className='navbar_cus_main_btn  theme_color_two' to='/about' style={{ fontWeight: 'bolder ' } }>Learn More</Link>
