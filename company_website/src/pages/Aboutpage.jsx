bef8f95724115a9dec1022a672ff39748c24a68e
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-09-23 17:41:47 +0530

    Update Aboutpage.jsx

--- company_website/src/pages/Aboutpage.jsx
+++ company_website/src/pages/Aboutpage.jsx
@@ -5,7 +5,7 @@ import Mission from '../components/Mission';
 import Corevalues from '../components/Corevalues';
 import Review from '../components/Review';
 import Footer from '../components/Footer'
-import Contactbanner from '../assets/contactbanner.jpg'
+import Contactbanner from '../assets/aboutusbanner.png'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import React, { useState, useEffect } from "react";
