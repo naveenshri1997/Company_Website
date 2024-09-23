caddff82e2d9bf1b851b0b8f41334a67ebcc6bef
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-09-23 17:15:14 +0530

    Update BreadCruums.jsx

--- company_website/src/components/BreadCruums.jsx
+++ company_website/src/components/BreadCruums.jsx
@@ -2,9 +2,9 @@ import bread from '../assets/breadcrums.jpg'
 const BreadCruums = ({ data }) => {
     return (
         <>            
-            <div className="container-fluid breadcrum" style={{ bread? backgroundImage:`url('${bread}')` : 
+            <div className="container-fluid breadcrum" style={ bread? backgroundImage:`url('${bread}')` : 
             backgroundImage:`url('${data.image}')`
-            }}>
+            }>
                 <div className="row justify-content-center" style={{ position: 'relative', zIndex: '1' }}>
                     <div className="col-lg-10">
                         <h1 class="heading" style={{ textAlign: 'left', fontWeight: "bolder", fontSize: '48px', color: 'white' }}>{data.pagename}</h1> 
