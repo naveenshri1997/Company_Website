e76c9252aff3a15c587b899541fda87e85c7f585
naveenshri1997 <naveenshri0546@gmail.com>
2024-10-08 02:01:53 +0530

    vf

--- company_website/src/pages/Careerpage.jsx
+++ company_website/src/pages/Careerpage.jsx
@@ -31,7 +31,7 @@ const Careerpage = () => {
         <>
             <BreadCruums data={data}/>
             <Navbar />
-            //<Jobs />
+            <Jobs />
             <Footer />
         </>
     )
