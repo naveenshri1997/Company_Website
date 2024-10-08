752939ecbf7225f42fd94efbb5073d452916dbae
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-10-08 13:23:13 +0530

    Update Jobs.jsx

--- company_website/src/components/Jobs.jsx
+++ company_website/src/components/Jobs.jsx
@@ -1,31 +1,48 @@
 import { Link } from 'react-router-dom'
-import React from "react";
-const Jobs = ({job}) => {   
+import React, { useState, useEffect } from "react";
+const Jobs = () => { 
+    const [jobs, setjobs] = useState([]);
+    useEffect(() => {
+        const res = fetch('https://whitewebtech.onrender.com/api/Jobs/GetAll', {
+            method: 'GET',
+        }).then((res) => res.json()).then((data) => {
+            console.log('data', JSON.stringify(data.result));
+            setjobs(data.result);
+        })
+    }, [])
+    
     return (
         <>
             <div className="container-fluid jobs">               
-                {/*<ul>*/}
-                {/*{props.items.map((item, index) => (*/}
-                {/*    <li key={index}>{item}</li>*/}
-                {/*))}*/}
-                {/*</ul>*/}
+                
                 <div className="row justify-content-center">
                     <div className="col-lg-10">
                         <h1 data-aos="fade-up" data-aos-duration="1600" className="mobile_heading londrina heading theme_color_two"
                             style={{ fontWeight: 'bolder ', color: 'black', padding: '0px 10px' }}>Current Jobs<span
                                 style={{ color: '#f84525' }}> Openings.</span></h1>
-                        <div className="card job-card" style={{marginTop:"50px"}}>
-                            <div className="row">
-                                <div className="col-lg-8">
-                                    <h4 className="title">{job.name}</h4>
-                                    <h4 className="subtitle">{job.title}</h4>
-                                    <h4 className="desc">{job.description}</h4>
-                                </div>
-                                <span className="col-lg-4 float-right" style={{marginTop:'15px'}}> 
-                                    <Link class="nav-link" to='/formpage'>Apply Now</Link>
-                                </span>
-                            </div>
-                        </div>
+
+                        {
+                            jobs.map((item) => {
+                                return (
+                                    <>
+                                        <div className="card job-card" style={{ marginTop: "50px" }}>
+                                            <div className="row">
+                                                <div className="col-lg-8">
+                                                    <h4 className="title">{item.name}</h4>
+                                                    <h4 className="subtitle">{item.title}</h4>
+                                                    <h4 className="desc">{item.description}</h4>
+                                                </div>
+                                                <span className="col-lg-4 float-right" style={{ marginTop: '15px' }}>
+                                                    <Link class="nav-link" to={`/career/${item.id}`} >{item.id}</Link>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </>
+                                )
+                            })
+                        }
+
+                       
                     </div>
                 </div>
             </div>
