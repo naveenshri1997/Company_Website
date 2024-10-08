19d57a459ddcfe9c8ac6a07ff42b964810e348de
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-10-08 15:10:08 +0530

    Create thankyou.jsx

---
+++ company_website/src/pages/thankyou.jsx
@@ -0,0 +1,42 @@
+import Navbar from '../components/Navbar';
+import BreadCruums from '../components/BreadCruums';
+import Aboutus from '../components/Aboutus';
+import Mission from '../components/Mission';
+import Corevalues from '../components/Corevalues';
+import Review from '../components/Review';
+import Footer from '../components/Footer'
+import Contactbanner from '../assets/aboutusbanner.png'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import React, { useState, useEffect } from "react";
+
+const thankyou = () => {
+
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+    }, []);
+    const data = {
+        pagetitle: "Home",
+        pagename: "Thank You",
+        image: `${Contactbanner}`
+    }
+    return (
+        <>
+
+            <Navbar />
+
+            <BreadCruums data={data} />
+            <div className="container-fluid jobs">
+
+                <div className="row justify-content-center">
+                    <div className="col-lg-10">
+                    <h1> THANKYOU </h1>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
+export default thankyou;
